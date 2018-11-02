78409bf1f8fd204426071a42003b582d2567816d
Marcello <marcel.buehler@uzh.ch>
2018-11-02 12:07:19 +0100

    setting default value for CLEAR_PREVIOUS to False

--- scripts/load_environment_variables.sh
+++ scripts/load_environment_variables.sh
@@ -2,4 +2,4 @@
 export DSLAB_CLIMATE_BASE_INPUT=/mnt/ds3lab-scratch/dslab2018/bernatj/model
 export DSLAB_CLIMATE_BASE_OUTPUT=/mnt/ds3lab-scratch/climate/preprocessed_output
 export DSLAB_N_JOBS=12
-export DSLAB_CLEAR_PREVIOUS=1
+export DSLAB_CLEAR_PREVIOUS=0
