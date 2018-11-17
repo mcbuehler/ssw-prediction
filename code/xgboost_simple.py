f17ccd79da0a583e3031293160b5e1f2adf4589b
Dimitris Koutsoukos <dkoutsou@student.ethz.ch>
2018-11-17 15:57:02 +0100

    Code fixes
    - Use DataManager
    - Split preprocessing functions into steps
    - Add more comments to make code more understandable
    - Split plotting from training

--- code/xgboost_simple.py
+++ code/xgboost_simple.py
@@ -1,8 +1,8 @@
-import h5py
 import numpy as np
 import pickle
 import pandas as pd
 import argparse
+from data_manager import DataManager
 from tsfresh import extract_features
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -24,7 +24,7 @@ class ManualAndXGBoost:
     features = ['wind_60', 'wind_65', 'temp_60_90']
 
     def __init__(self, definition, path, pickle_path):
-        """the constructor of the ManualAndXgboost class
+        """The constructor of the ManualAndXgboost class
 
         Parameters
         ----------
@@ -39,51 +39,125 @@ class ManualAndXGBoost:
         self.definition = definition
         self.path = path
         self.pickle_path = pickle_path
+        self.data_manager = DataManager(self.path)
 
-    def get_labels_variables(self, only_labels):
-        """Gets the data and according to the definition that the classification
-        should be done for extracts the relevant labels and the features
+    def __get_labels(self):
+        """Returns the binary labels as a numpy array for the corresponding
+        definition
 
-        Parameters
-        ----------
-            only_labels: boolean
-                A flag to decide if you are going to return both labels and
-                features or just labels
+        Returns
+        -------
+            labels: numpy array
+                A numpy array of size [num_data x 1] with the labels
+        """
+        labels = self.data_manager.get_data_for_variable(self.definition)
+        binary_labels = []
+        for label in labels:
+            binary_labels.append(int(np.any(label[:])))
+        labels = np.array(binary_labels)
+        return labels
+
+    def __bring_data_to_format(self):
+        """Gets the corresponding initial variables using the data manager class
+        and returns a data numpy array that the features in format
+        [num_data*num_features, len_winter]. The trick here is that
+        consecutive lines are not of the same feature space but from different
+        features
         Returns
         -------
             data: numpy array
-                A numpy array of the relevant features for the definition used
-            labels: numpy array
-                A numpy array of the relevant labels for the definition used
-        """
+                A numpy array of format [num_data*num_features, len_winter]
+            """
+        temp_data = []
+        for feature in self.features:
+            temp_data.append(self.data_manager.get_data_for_variable(
+                             feature))
+
+        temp_data = np.array(temp_data)
+        num_variables = len(self.features)
+        num_data = temp_data[0].shape[0]
         data = []
-        labels = []
-
-        with h5py.File(self.path, 'r') as f:
-            for winter, timeseries in f.items():
-                for key, value in timeseries.items():
-                    if key == self.definition:
-                        labels.append(int(np.any(value[:])))
-
-                    if not only_labels:
-                        if key in self.features:
-                            data.append(np.array(value[:]))
-
+        # The tricky part here is that now we have a 3D tensor for the three
+        # variables but it is actually better to convert it to a 2D tensor that
+        # will have the 3 features in 3 consecutive lines instead of having
+        # them with distance num_data (in our case ~3000)
+        for i in range(num_data):
+            for j in range(num_variables):
+                data.append(temp_data[j, i, :])
         data = np.array(data)
-        labels = np.array(labels)
+        # testing that the transformation was done correctly
+        # for i in range(num_data*3):
+        #     print(np.array_equal(data[i], temp_data[i % 3][int(i/3)]))
+        # sys.exit(0)
+        return data
 
-        if only_labels:
-            return labels
-        else:
-            return data, labels
+    def __produce_features(self, data):
+        """
+        Gets the data in the format [num_data*num_features, len_winter] and by
+        using tsfresh it produces a matrix [num_data,
+        tsfresh_features*num_variables]. It also creates a list of
+        [tsfresh_features*num_variables] to have the correspondence between the
+        initial variables and the features produced and saves that in a class
+        variable.
+
+        Returns
+        -------
+            features: np.array
+                A numpy array of size
+                [num_data, tsfresh_features*num_variables]
+        """
+        length = data.shape[1]
+        features = []
+        flag = True
+        # iterate though all the data points
+        for i, row in enumerate(data):
+            # bring them into the format the tsfresh wants them to compute the
+            # features
+            for_tsfresh = pd.DataFrame({'id': np.ones(length),
+                                        'time': np.arange(length),
+                                        'value': row})
+            X = extract_features(for_tsfresh, column_id='id',
+                                 column_sort='time')
+            # in the first iteration store the corresponding names of the the
+            # features as well. You have to expand the features for one
+            # variable to prepend the name of the variable and also do that
+            # for all the variables.
+            if flag:
+                temp_feature_keys = X.columns.values
+                flag = False
+                self.feature_keys = []
+                keys_features_length = len(temp_feature_keys)
+                features_length = len(self.features)
+                for i in range(keys_features_length*features_length):
+                    quotient = int(i / keys_features_length)
+                    self.feature_keys.append(
+                            self.features[quotient] + "_" +
+                            temp_feature_keys[
+                                i-quotient*keys_features_length]
+                            )
+
+            features.append(X.values[0])
+
+        # bring the features from format [num_data*num_features, len_winter] to
+        # [num_data, num_features*len_winter]
+        new_features = []
+        for i, feature in enumerate(features):
+            if i % 3 == 2:
+                new_features.append(np.concatenate((features[i],
+                                    features[i-1], features[i-2]),
+                                    axis=0))
+
+        features = new_features[:]
+        features = np.asarray(features)
+        return features
 
     def preprocess(self):
         """This function produces the train and the test split of the features
-        as well as the train and the test split of the labels. Multiple things
-        are happening. First of all it tries to read the already processed
-        features and their labels from tsfresh from a pickle file. If they are
-        found available it uses them instead of calculating them again. After
-        that is splits the dataset into test and train.
+        as well as the train and the test split of the labels. In order to do
+        that it uses other functions to produce the labels and the features. It
+        first checks if the labels are in the corresponding folder provided as
+        an argument to the class and if not it produces them. Then it does the
+        splitting.
 
         Returns
         -------
@@ -97,49 +171,17 @@ class ManualAndXGBoost:
             y_test: numpy array
                 A numpy array of shape [num_data x 1] the test labels
         """
+        labels = self.__get_labels()
         try:
             with open(str(self.pickle_path), 'rb') as f:
                 features, self.feature_keys = pickle.load(f)
             features = np.array(features)
-            labels = self.get_labels_variables(only_labels=True)
         except FileNotFoundError:
             print("Didn't find the .pkl file of the features. Producing it",
                   "now, under the pickle path folder")
-            data, labels = self.get_labels_variables(only_labels=False)
-            length = data.shape[1]
-            features = []
-            flag = True
-            for i, row in enumerate(data):
-                for_tsfresh = pd.DataFrame({'id': np.ones(length),
-                                            'time': np.arange(length),
-                                            'value': row})
-                X = extract_features(for_tsfresh, column_id='id',
-                                     column_sort='time')
-                if flag:
-                    temp_feature_keys = X.columns.values
-                    flag = False
-                    self.feature_keys = []
-                    keys_features_length = len(temp_feature_keys)
-                    features_length = len(self.features)
-                    for i in range(keys_features_length*features_length):
-                        quotient = int(i / keys_features_length)
-                        self.feature_keys.append(
-                                self.features[quotient] + "_" +
-                                temp_feature_keys[
-                                    i-quotient*keys_features_length]
-                                )
 
-                features.append(X.values[0])
-
-            new_features = []
-            for i, feature in enumerate(features):
-                if i % 3 == 2:
-                    new_features.append(np.concatenate((features[i],
-                                        features[i-1], features[i-2]),
-                                        axis=0))
-
-            features = new_features[:]
-            features = np.asarray(features)
+            data = self.__bring_data_to_format()
+            features = self.__produce_features(data)
             with open(str(self.pickle_path), 'wb') as f:
                 pickle.dump([features, self.feature_keys], f)
 
@@ -166,6 +208,17 @@ class ManualAndXGBoost:
         """
         model = XGBClassifier()
         model.fit(X_train, y_train)
+        return model
+
+    def plot(self, model):
+        """ Prints the three most important features for the classification
+        when the XGB Classifier has been used and plots them as a bar plot.
+        Parameters
+        ----------
+            model: XGBClassifier class
+                A trained model
+        """
+
         top_3 = sorted(model.feature_importances_, reverse=True)[:3]
         max_idxs = []
         for i in range(3):
@@ -177,7 +230,6 @@ class ManualAndXGBoost:
         pyplot.title("Feature importance for definition:" + self.definition)
         pyplot.bar(list(range(1, 4)), top_3)
         pyplot.show()
-        return model
 
     def test(self, model, X_test, y_test):
         """Returns the accuracy of the model on the test set.
@@ -215,7 +267,7 @@ if __name__ == '__main__':
             "--input_path",
             help="Choose the input path where the data are",
             action="store",
-            default="data/data_preprocessed_labeled.h5"
+            default="data/data_labeled.h5"
             )
     parser.add_argument(
             "-o",
@@ -235,3 +287,4 @@ if __name__ == '__main__':
     model = test.train(X_train, y_train)
     accuracy = test.test(model, X_test, y_test)
     print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    test.plot(model)
