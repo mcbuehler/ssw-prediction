a15b9ec73a0040d110578d1f8118c130387aed29
Dimitris Koutsoukos <dkoutsou@student.ethz.ch>
2018-11-22 12:56:57 +0100

    Add script to set up the GPU

---
+++ code/utils/set_gpu.py
@@ -0,0 +1,15 @@
+import os
+import gpustat
+
+
+def set_gpu():
+    """Code by: https://github.com/bamos/setGPU/blob/master/setGPU.py"""
+    stats = gpustat.GPUStatCollection.new_query()
+    ids = map(lambda gpu: int(gpu.entry['index']), stats)
+    ratios = map(lambda gpu: float(gpu.entry['memory.used']) /
+                 float(gpu.entry['memory.total']), stats)
+    bestGPU = min(zip(ids, ratios), key=lambda x: x[1])[0]
+
+    print("setGPU: Setting GPU to: {}".format(bestGPU))
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(bestGPU)
