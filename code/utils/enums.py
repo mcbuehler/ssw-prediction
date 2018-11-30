fbc0a4126b1124e405ddb102fdf07016c1857bc6
Dimitris Koutsoukos <dkoutsou@student.ethz.ch>
2018-11-30 11:03:39 +0100

    Add enums needed for better overview of the project

---
+++ code/utils/enums.py
@@ -0,0 +1,19 @@
+class Task:
+    """An enum class for the task"""
+    prediction = "prediction"
+    classification = "classification"
+
+
+class Metric:
+    """An enum class for the evaluation metric"""
+    auroc = "auroc"
+    f1 = "f1"
+    accuracy = "accuracy"
+
+
+class Classifier:
+    """An enum class for the classifier used"""
+    xgboost = "xgboost"
+    cnn = "cnn"
+    rnn = "rnn"
+    randomforest = "randomforest"
