5329b22da4d4bdbd6fb1d646321a37139ba56495
Dimitris Koutsoukos <dkoutsou@student.ethz.ch>
2018-11-30 11:03:17 +0100

    Make changes requested by @mbuehler

--- code/utils/output_class.py
+++ code/utils/output_class.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from enums import Task, Metric, Classifier
+from preprocessing.dataset import DatapointKey
 
 
 class Output:
@@ -8,7 +10,7 @@ class Output:
     tasks = ['prediction', 'classification']
 
     def __init__(self, classifier, task, definition, cutoff_point,
-                 feature_interval, prediction_interval, metric, result):
+                 feature_interval, prediction_interval, metric, scores):
         """Constructor of the output class
 
         :classifier: The classifier that has been used for the task
@@ -20,14 +22,14 @@ class Output:
         :prediction_interval: The number of days in the future you want to
                               predict for
         :metric: The metric used (accuracy, auroc, f1)
-        :result: The result of the experiment
+        :scores: The scores of the experiment as a python list
         """
-        assert task in self.tasks, ("The available tasks are 'prediction' and"
-                                    "'classification'")
-        assert definition in self.definitions, ("The available definitions are"
-                                                "'CP07', 'U&T', 'U65'")
-        assert metric in self.metrics, ("The available metrics are 'accuracy',"
-                                        "'auroc', 'f1'")
+        assert task in self.tasks, (
+                "The available tasks are 'prediction' and 'classification'")
+        assert definition in self.definitions, (
+                "The available definitions are 'CP07', 'U&T', 'U65'")
+        assert metric in self.metrics, (
+                "The available metrics are 'accuracy','auroc', 'f1'")
 
         self.classifier = classifier
         self.task = task
@@ -36,7 +38,7 @@ class Output:
         self.feature_interval = feature_interval
         self.prediction_interval = prediction_interval
         self.metric = metric
-        self.result = result
+        self.scores = scores
 
     def write_output(self):
         """Writes the output of the experiment into a CSV file"""
@@ -46,29 +48,22 @@ class Output:
                   "to the 'results/results.csv' file in the repo")
             sys.exit(1)
 
-        # Checks for types and converts if necessary
-        if isinstance(self.cutoff_point, int):
-            self.cutoff_point = str(self.cutoff_point)
-        if isinstance(self.feature_interval, int):
-            self.feature_interval = str(self.feature_interval)
-        if isinstance(self.prediction_interval, int):
-            self.prediction_interval = str(self.prediction_interval)
-        if isinstance(self.result, float):
-            self.result = str(self.result)
-
-        output_string = (
-                self.classifier + "," +
-                self.task + "," +
-                self.cutoff_point + "," +
-                self.feature_interval + "," +
-                self.prediction_interval + "," +
-                self.metric + "," +
-                self.result + "\n"
+        scores = ','.join(str(e) for e in self.scores)
+        output_string = "{},{},{},{},{},{},{},{}".format(
+                self.classifier,
+                self.task,
+                self.definition,
+                self.cutoff_point,
+                self.feature_interval,
+                self.prediction_interval,
+                self.metric,
+                scores
                 )
         with open(path_result_file, 'a') as csv_file:
             csv_file.write(output_string)
 
 
 if __name__ == "__main__":
-    test = Output('XGBoost', 'prediction', 'CP07', 120, 60, 30, 'auroc', 0.78)
+    test = Output(Classifier.xgboost, Task.prediction, DatapointKey.CP07, 120,
+                  60, 30, Metric.auroc, 5*[0.78])
     test.write_output()
