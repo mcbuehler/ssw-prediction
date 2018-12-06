85c8b32478ec19fcc72c1991ead2c75b7d64487a
Dimitris Koutsoukos <dkoutsou@student.ethz.ch>
2018-12-06 11:46:21 +0100

    Change running script

--- code/run.sh
+++ code/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for cutoff in 60 90
+for cutoff in 90 120
 do
     for definition in CP07 U\&T U65
     do
-        for feature in 30 40 50
+        for feature in 7 14 21 28
         do
-            for prediction in 5 10 15 20 25 30
+            for prediction in 1 2 3 4
             do
                 #echo "Definition:$definition-Cutoff:$cutoff-Prediction:$prediction"
                 python code/prediction/xgboost_prediction.py -d $definition -cp $cutoff -pi $prediction -fi $feature >> results.txt& 
