8f6e155790eab2c2ab2fec77da94f6c1f7a09275
Marcello <marcel.buehler@uzh.ch>
2018-12-08 14:00:31 +0100

    setup prediction experiments for simulated data

--- code/prediction/run_randomforest_prediction.py
+++ code/prediction/run_randomforest_prediction.py
@@ -12,33 +12,37 @@ def run_experiment_grid_prediction():
     cutoff_points = [90, 120]
     feature_intervals = [7, 14, 21, 28]
     prediction_intervals = [7, 14, 21, 28]
+    prediction_start_days = [0, 7, 14, 21]
 
     # Evaluate for simulated data only
     for definition in definitions:
         for cutoff in cutoff_points:
             for feature in feature_intervals:
                 for prediction in prediction_intervals:
-                    print("Evaluating for "
-                          "definition {}, "
-                          "cutoff {}, "
-                          "feature {}, "
-                          "prediction {}".format(
-                            definition,
-                            cutoff,
-                            feature,
-                            prediction))
+                    for start_day in prediction_start_days:
+                        print("Evaluating for "
+                              "definition {}, "
+                              "cutoff {}, "
+                              "feature {}, "
+                              "prediction {},"
+                              "start day {}".format(
+                                definition,
+                                cutoff,
+                                feature,
+                                prediction,
+                                start_day))
 
-                    model = RandomForestPrediction(
-                        definition=definition,
-                        path=input_path_sim,
-                        n_bins=n_bins,
-                        n_estimators=n_estimators,
-                        cutoff_point=cutoff,
-                        features_interval=feature,
-                        prediction_interval=prediction
-                    )
+                        model = RandomForestPrediction(
+                            definition=definition,
+                            path=input_path_sim,
+                            n_bins=n_bins,
+                            n_estimators=n_estimators,
+                            cutoff_point=cutoff,
+                            features_interval=feature,
+                            prediction_interval=prediction
+                        )
 
-                    model.evaluate(plot=False)
+                        model.evaluate(plot=False)
 
 
 if __name__ == '__main__':
